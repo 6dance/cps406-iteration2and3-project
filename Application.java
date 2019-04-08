462cf516b8933bf32dc387956fdf58b08fd3015d
NA <naween803@gmail.com>
2019-04-07 23:27:27 -0400

    initial menu bar

--- Application.java
+++ Application.java
@@ -6,17 +6,45 @@ public class Application
 {
     public static void main(String[] args) throws Exception
     {
-        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         initUI();
     }
 
     private static void initUI()
     {
+        // Create window UI display
         JFrame display = new JFrame("Music Manager - CPS406")
         {
             {
+                // Create toolbar menu
+                JMenuBar MenuBar = new JMenuBar();
+
+                // Add "File" menu option
+                MenuBar.add(new JMenu("File")
+                {
+                    {
+                        // Add File options
+                        add(new JMenuItem("Add...")
+                        {
+                            {
+                                addActionListener((x) -> System.exit(0));
+                            }
+                        });
+                        add(new JSeparator());
+                        add(new JMenuItem("Exit")
+                        {
+                            {
+                                addActionListener((x) -> System.exit(0));
+                            }
+                        });
+                    }
+                });
+
                 setSize(new Dimension(800, 600));
+                setLayout(new BorderLayout());
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                add(MenuBar, BorderLayout.NORTH);
+                
                 setVisible(true);
             }
         };
