eb3c0e864b009ed7ea81d0da4c95812136f02647
NA <naween803@gmail.com>
2019-04-08 22:03:20 -0400

    more UI, table

--- Application.java
+++ Application.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-
 public class Application
 {
     public static void main(String[] args) throws Exception
@@ -16,34 +15,56 @@ public class Application
         JFrame display = new JFrame("Music Manager - CPS406")
         {
             {
-                // Create toolbar menu
-                JMenuBar MenuBar = new JMenuBar();
-
-                // Add "File" menu option
-                MenuBar.add(new JMenu("File")
+                setSize(new Dimension(800, 600));
+                setLayout(new BorderLayout());
+                
+                // Create top toolbar menu
+                add(new JMenuBar()
                 {
                     {
-                        // Add File options
-                        add(new JMenuItem("Add...")
+                        // Add "File" menu option
+                        add(new JMenu("File")
                         {
                             {
-                                addActionListener((x) -> System.exit(0));
-                            }
-                        });
-                        add(new JSeparator());
-                        add(new JMenuItem("Exit")
-                        {
-                            {
-                                addActionListener((x) -> System.exit(0));
+                                // Add File options
+                                add(new JMenuItem("Add...")
+                                {
+                                    {
+                                        addActionListener((x) -> System.exit(0));
+                                    }
+                                });
+                                add(new JSeparator());
+                                add(new JMenuItem("Exit")
+                                {
+                                    {
+                                        addActionListener((x) -> System.exit(0));
+                                    }
+                                });
                             }
                         });
                     }
-                });
+                }, BorderLayout.NORTH);
 
-                setSize(new Dimension(800, 600));
-                setLayout(new BorderLayout());
+                // Create bottom toolbar menu
+                add(new JMenuBar()
+                {
+                    {
+                        add(new JLabel("User: "));
+                        add(new JComboBox());
+
+                        add(new JLabel("Currently viewing: "));
+                        add(new JComboBox());
+
+                        add(new JButton("New playlist"));
+                    }
+                }, BorderLayout.SOUTH);
+
+                // Create centre table which displays songs in database/playlist
+                final String TABLE_HEADERS[] = {"Name", "Length", "Artist", "Genre"};
+                Object TABLE_DATA[][] = { {"", "", "", ""} };
+                add(new JScrollPane(new JTable(TABLE_DATA, TABLE_HEADERS)), BorderLayout.CENTER);
+
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                add(MenuBar, BorderLayout.NORTH);
                 
                 setVisible(true);
             }
