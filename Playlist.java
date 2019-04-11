1b16745c11f298ccf81d28d3024d94a56c4db367
Samuel Bahri <31296340+samuelbahri@users.noreply.github.com>
2019-04-10 23:15:12 -0400

    Create Playlist.java
    Playlist tasks 1 and 2, internal workings

---
+++ Playlist.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class Playlist{
+        LinkedList<Song> playlist = new LinkedList<Song>();
+        String playlistName;
+        int playlistTime;
+        
+        //Returns playlist
+        public LinkedList<Song> getPlaylist() {
+            return this.playlist;
+        }
+        
+        //Returns playlist name
+	public String getName() {
+		return this.playlistName;
+	}
+	
+	//Sets Username of user
+	public void setName(String username) {
+		this.playlistName = username;
+	}
+        
+        //Adds new song to playlist
+        public void addToPlaylist(Song song){
+            playlist.add(song);
+            playlistTime = playlistTime + song.getLength();
+        }
+        
+        public void removeFromPlaylist (Song song){
+            playlist.remove(song);
+            playlistTime = playlistTime - song.getLength();
+        }
+}
